e6a71eb8b9d3941f15dcd1b194e4e680c6888126
Igor Korchagin <igor.korchagin8@gmail.com>
2022-10-08 23:56:25 +0200

    rm redundant types and upgrade gc-json-logger-express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './create-nest-logger';
-export * from './log-entry.interface';
 export * from './logger.module';
 export * from './logger.module-options';
 export * from './logger.service';
--- src/log-entry.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Severity } from 'gc-json-logger';
-import { LogEntryMetadata } from 'gc-json-logger-express';
-
-export interface LogEntry {
-  severity: Severity;
-  message: string;
-  meta?: LogEntryMetadata;
-}

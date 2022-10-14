a3a320067847e80aff4df46111e97f0b75f5186f
Igor Korchagin <igor.korchagin8@gmail.com>
2022-10-14 18:38:38 +0200

    allow making logger global module

--- src/logger.module-definition.ts
+++ src/logger.module-definition.ts
@@ -2,4 +2,6 @@ import { ConfigurableModuleBuilder } from '@nestjs/common';
 import { LoggerModuleOptions } from './logger.module-options';
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE, ASYNC_OPTIONS_TYPE } =
-  new ConfigurableModuleBuilder<LoggerModuleOptions>().build();
+  new ConfigurableModuleBuilder<LoggerModuleOptions>()
+    .setExtras({ global: true }, (definition, extras) => ({ ...definition, global: extras.global }))
+    .build();
